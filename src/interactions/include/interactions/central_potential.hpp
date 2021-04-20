7ad87d26fd0135405d84c468b846650b150adbfa
Kai Szuttor <kai@icp.uni-stuttgart.de>
2021-04-20 11:28:13 +0200

    inter: beautification.

--- src/interactions/include/interactions/central_potential.hpp
+++ src/interactions/include/interactions/central_potential.hpp
@@ -7,28 +7,28 @@ template <class Inter> struct CentralPotential {
   using value_type = typename Inter::value_type;
   using vector_type = typename Inter::vector_type;
 
-  value_type m_cutoff;
-  value_type m_offset;
-  value_type m_shift;
-  Inter m_inter;
+  value_type cut;
+  value_type offset;
+  value_type shift;
+  Inter inter;
 
   CentralPotential(value_type cutoff, value_type offset, value_type shift)
-      : m_cutoff(cutoff), m_offset(offset), m_shift(shift){};
+      : cut(cutoff), offset(offset), shift(shift){};
 
   bool in_range(value_type r) const {
-    auto const r_eff = r - m_offset;
-    return r_eff <= m_cutoff and r_eff > 0.0;
+    auto const r_eff = r - offset;
+    return r_eff <= cut and r_eff > 0.0;
   }
 
   value_type energy(value_type r) const {
-    return in_range(r) ? m_inter.energy(r - m_offset) + m_shift : value_type{};
+    return in_range(r) ? inter.energy(r - offset) + shift : value_type{};
   }
 
   vector_type force(value_type r, vector_type const &r12) const {
-    return in_range(r) ? m_inter.force(r - m_offset, r12) : vector_type{};
+    return in_range(r) ? inter.force(r - offset, r12) : vector_type{};
   }
 
-  value_type cutoff() const { return m_cutoff; }
+  value_type cutoff() const { return cut; }
 };
 
 } // namespace Interactions
